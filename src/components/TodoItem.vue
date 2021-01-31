f2d78f0aaa0e901bd5ee3ff1d1bcfb56e6436257
balhayre <baljinder.hayre@live.co.uk>
2021-01-31 17:24:13 +0000

    First attempt at VueJS - simple to Do list

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+     <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ["todo"]
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .todo-item {
+    background: cadetblue;
+    padding: 10px;
+    border-bottom: 1px wheat dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: red;
+    color: white;
+    border: none;
+    padding: 5px 9px;
+    cursor: pointer;
+    float: right;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="hello">
-     <h1>How you doing??</h1>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" />
+    </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"] 
 }
 </script>
 
